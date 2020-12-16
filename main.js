7aeeb56352b0a5f4aa0c4673e2b00c1da5bab184
Enin Fujimi <tkgm45nbchk@gmail.com>
2020-12-16 23:30:53 +0900

    This is first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+// エラーログ用
+const nameOfComponent = "mondayTimer/index";
+
+// 現在時刻の取得
+const getCurrentTime = () => {
+	const now = new Date();
+	return { 
+        day: now.getDay(),
+	    hour: now.getHours(),
+	    min: now.getMinutes(),
+        sec: now.getSeconds(),
+    }
+}
+
+// 月曜なら 0:00:00, そうでなければ残り時間を計算
+const getRemain = (obj) => {
+    if(obj.day === 1) {
+        return { hour: 0, min: "00", sec: "00" };
+    } else {
+        return calcRemain(obj);
+    }
+}
+
+// 月曜日までの残り日数を取得 
+const getD = (day) => {
+    if(day > 0) {
+        return 7;
+    } else {
+        return 0;
+    }
+}
+
+// 月曜日までの残り時間を取得
+const getH = (obj) => {
+    const day = (getD(obj.day) - obj.day) * 24;
+    let hour = 24 - obj.hour;
+    if(obj.min > 0) hour--;
+    if (day >= 24) hour += day;
+    return hour;
+}
+
+// 月曜日までの残り時間（分）を取得
+const getM = (obj) => {
+    let min = 60 - obj.min;
+	if (obj.sec > 0) min--;
+    if (min === 60) min = 0;
+    if (min < 10) min = "0" + min;
+    return min;
+}
+
+// 月曜日までの残り時間（秒）を取得
+const getS = (obj) => {
+    let sec = 60 - obj.sec;
+    if (sec === 60) {
+        return "00";
+    } else if(sec > 9) {
+        return sec;
+    } else {
+        return "0" + sec;
+    }
+}
+
+// 残り時間（時間、分、秒）を計算して出力
+const calcRemain = (obj) => {
+    const hour = getH(obj);
+    const min = getM(obj);
+    const sec = getS(obj);
+    return { hour: hour, min: min, sec: sec };
+}
+
+const MondayTimer = (props) => {
+    const [hms, setHms] = useState(getRemain(getCurrentTime()));
+    useEffect(() => {
+        const time = setInterval(() => {
+            setHms(getRemain(getCurrentTime()));
+        }, 1000);
+        return () => clearInterval(time);
+    }, []);
+
+    return (
+        <div className="container tool mondayTimer">
+            <h1>月曜日まであと</h1>
+            <h2>
+                <span className="time">{ hms.hour }</span>
+                <span className="words">時間</span>
+                <span className="time">{ hms.min }</span>
+                <span className="words">分</span>
+                <span className="time">{ hms.sec }</span>
+                <span className="words">秒です。</span>
+            </h2>
+        </div>
+    );
+}
+
+export default MondayTimer;
